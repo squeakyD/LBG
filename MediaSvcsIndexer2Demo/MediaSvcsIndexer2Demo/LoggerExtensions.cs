9803425fe49253e90a90338b0a220076ef599df4
Martin Davies <martin.davies@bjss.com>
2017-05-23 18:17:22 +0100

    More logging, attempt at batch processing and encryption

---
+++ MediaSvcsIndexer2Demo/MediaSvcsIndexer2Demo/LoggerExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Microsoft.WindowsAzure.MediaServices.Client;
+
+namespace MediaSvcsIndexer2Demo
+{
+    public static class LoggerExtensions
+    {
+        public static object ToLog(this IJob obj)
+        {
+            return new { obj.Name, obj.Id };
+        }
+        public static object ToLog(this IAsset obj)
+        {
+            return new {obj.Name, obj.Id, ContentKeyCount = obj.ContentKeys?.Count ?? 0};
+        }
+        public static object ToLog(this IAssetFile obj)
+        {
+            return new {obj.Name, obj.Id, obj.EncryptionScheme, obj.IsEncrypted};
+        }
+        public static object ToLog(this ITask obj)
+        {
+            return new { obj.Name, obj.Id, obj.EncryptionScheme };
+        }
+    }
+
+    //public static class ExtensionMethods
+    //{
+    //    // Deep clone
+    //    public static T DeepClone<T>(this T a)
+    //    {
+    //        using (MemoryStream stream = new MemoryStream())
+    //        {
+    //            BinaryFormatter formatter = new BinaryFormatter();
+    //            formatter.Serialize(stream, a);
+    //            stream.Position = 0;
+    //            return (T)formatter.Deserialize(stream);
+    //        }
+    //    }
+    //}
+}
--- MediaSvcsIndexer2Demo/MediaSvcsIndexer2Demo/Program.cs
+++ MediaSvcsIndexer2Demo/MediaSvcsIndexer2Demo/Program.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.WindowsAzure.MediaServices.Client;
+using Microsoft.WindowsAzure.MediaServices.Client.Metadata;
 using Serilog;
 
 namespace MediaSvcsIndexer2Demo
@@ -49,14 +50,32 @@ namespace MediaSvcsIndexer2Demo
                     {
                         _delFiles = true;
                     }
+
+                    if (args[0].Equals("getAssets", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        GetAllAssetFiles();
+                        return;
+                    }
                 }
 
                 // Run indexing job.
-                string src = Path.Combine(_sourceDir, "4th Apr 17_612026009250275cut.wav");
+                //string src = Path.Combine(_sourceDir, "4th Apr 17_612026009250275cut.wav");
+                //string src = Path.Combine(_sourceDir, "612026009249280 040417 0932_1191101cut.wav");
+                //string src = Path.Combine(_sourceDir, "612026009249955 040417 1028_1191101cut.wav");
+                string src = Path.Combine(_sourceDir, "612026009250579 040417 1110_1191101cut.wav");
+                //string src5 = Path.Combine(_sourceDir, "612026009280132 110417_ATJStest1191100.wav");
+
+                _context.NumberOfConcurrentTransfers = 5;
+
                 var asset = RunIndexingJob(src, @"..\..\config.json");
 
                 // Download the job output asset.
                 DownloadAsset(asset, _outDir);
+
+                //foreach (var s in new[] {src,src2,src3,src4,src5})
+                //{
+                //    var asset = RunIndexingJob(src, @"..\..\config.json");
+                //}
 
                 if (_delFiles)
                 {
@@ -68,6 +87,32 @@ namespace MediaSvcsIndexer2Demo
                 _logger.Error(ex, "Fatal error in demo program!!!");
                 //Console.WriteLine(ex);
                 //throw;
+            }
+        }
+
+        private static void GetAllAssetFiles()
+        {
+            CreateCredentials();
+
+            _logger.Information("Downloading all stored audio file info");
+
+            foreach (var asset in _context.Assets)
+            {
+                var key = asset.ContentKeys.FirstOrDefault();
+                if (key == null)
+                {
+                    _logger.Information("Asset {asset} has no ContentKey.", asset.ToLog());
+                }
+                else
+                {
+                    _logger.Information("Asset {asset} has content key: {key} ", asset.ToLog(),
+                        new {key.ContentKeyType, key.EncryptedContentKey, key.ProtectionKeyId});
+                }
+
+                foreach (var af in asset.AssetFiles)
+                {
+                    _logger.Information("   Asset has file {file}", af.ToLog());
+                }
             }
         }
 
@@ -91,19 +136,24 @@ namespace MediaSvcsIndexer2Demo
         static IAsset RunIndexingJob(string inputMediaFilePath, string configurationFile)
         {
             _logger.Information("Running index job for {inputMediaFilePath}", inputMediaFilePath);
+            //_logger.Information("Running index job for {inputMediaFilePath}", _sourceDir);
 
             // Create an asset and upload the input media file to storage.
             IAsset asset = CreateAssetAndUploadSingleFile(inputMediaFilePath,
                 "My Indexing Input Asset",
-                AssetCreationOptions.None);
+                //AssetCreationOptions.None);
+                AssetCreationOptions.StorageEncrypted);
 
+            //var asset = CreateAssetFromFolder();
+
+            _logger.Debug("Creating indexing job");
             // Declare a new job.
             IJob job = _context.Jobs.Create("My Indexing Job");
 
             // Get a reference to Azure Media Indexer 2 Preview.
-            const string MediaProcessorName = "Azure Media Indexer 2 Preview";
 
-            var processor = GetLatestMediaProcessorByName(MediaProcessorName);
+            //var processor = GetLatestMediaProcessorByName(MediaProcessorNames.AzureMediaIndexer2Preview);
+            var processor = _context.MediaProcessors.GetLatestMediaProcessorByName(MediaProcessorNames.AzureMediaIndexer2Preview);
 
             // Read configuration from the specified file.
             string configuration = File.ReadAllText(configurationFile);
@@ -114,18 +164,18 @@ namespace MediaSvcsIndexer2Demo
                 configuration,
                 TaskOptions.None);
 
-            _logger.Debug("Created task {taskId} for job", task.Id);
+            _logger.Debug("Created task {task} for job", task.ToLog());
 
             // Specify the input asset to be indexed.
             task.InputAssets.Add(asset);
 
             // Add an output asset to contain the results of the job.
-            task.OutputAssets.AddNew("My Indexing Output Asset", AssetCreationOptions.None);
+            task.OutputAssets.AddNew("My Indexing Output Asset", AssetCreationOptions.StorageEncrypted);
 
             // Use the following event handler to check job progress.  
             job.StateChanged += new EventHandler<JobStateChangedEventArgs>(StateChanged);
 
-            _logger.Information("Submitted job {jobId}", job.Id);
+            _logger.Information("Submitted job {job}", job.ToLog());
 
             // Launch the job.
             job.Submit();
@@ -148,13 +198,48 @@ namespace MediaSvcsIndexer2Demo
             return job.OutputMediaAssets[0];
         }
 
+        private static IAsset CreateAssetFromFolder()
+        {
+            _logger.Debug("Creating asset and uploading all files in {folder}", _sourceDir);
+            var asset = _context.Assets.CreateFromFolder(_sourceDir, AssetCreationOptions.StorageEncrypted);
+
+            // Create a manifest file that contains all the asset file names and upload to storage.
+            _logger.Debug("Creating manifest");
+            string manifestFile = "input.lst";
+
+            var filenames = asset.AssetFiles.AsEnumerable().Select(f => f.Name);
+            File.WriteAllLines(manifestFile, filenames);
+            var assetFile = asset.AssetFiles.Create(Path.GetFileName(manifestFile));
+            assetFile.Upload(manifestFile);
+
+            var assetFile2 = asset.AssetFiles.Create(Path.GetFileName("indexerCfg.xml"));
+            assetFile2.Upload("indexerCfg.xml");
+            return asset;
+        }
+
+        private static IContentKey _contentKeyForFile = null;
+
         static IAsset CreateAssetAndUploadSingleFile(string filePath, string assetName, AssetCreationOptions options)
         {
             IAsset asset = _context.Assets.Create(assetName, options);
+            _logger.Debug("Created asset {asset}", asset.ToLog());
+
+            _contentKeyForFile = asset.ContentKeys.FirstOrDefault();
 
             var assetFile = asset.AssetFiles.Create(Path.GetFileName(filePath));
+
+            string iv=assetFile.InitializationVector;
+            string eid = assetFile.EncryptionKeyId;
+            
             assetFile.Upload(filePath);
-            _logger.Information("Uploaded {filePath}", filePath);
+            _logger.Information("Uploaded {filePath} to {assetFile}", filePath, assetFile.ToLog());
+            
+            asset.ContentKeys.Clear();
+            asset.Update();
+
+            //var test = asset.ContentKeys;
+
+            _logger.Debug("Removed asset key");
 
             return asset;
         }
@@ -164,19 +249,19 @@ namespace MediaSvcsIndexer2Demo
             _logger.Debug("Downloading files");
             foreach (IAssetFile file in asset.AssetFiles)
             {
-                _logger.Information($"Downloading {file.Name}");
+                _logger.Information("Downloading {file}", file.ToLog());
                 file.Download(Path.Combine(outputDirectory, file.Name));
 
                 if (_delFiles)
                 {
-                    _logger.Debug($"Deleting output file {file.Name} in asset {asset.Name}");
+                    _logger.Debug("Deleting output file {file} in asset {asset}", file.ToLog(), asset.ToLog());
                     file.Delete();
                 }
             }
 
             if (_delFiles)
             {
-                _logger.Debug($"Deleting output asset {asset.Name}");
+                _logger.Debug("Deleting output asset {asset}", asset.ToLog());
                 asset.Delete();
             }
         }
@@ -187,11 +272,11 @@ namespace MediaSvcsIndexer2Demo
             {
                 foreach (var af in asset.AssetFiles)
                 {
-                    _logger.Information("Deleting {file} from asset {asset}", af.Name, new {asset.Name, asset.Id});
+                    _logger.Information("Deleting {file} from asset {asset}", af.ToLog(), asset.ToLog());
                     af.Delete();
                 }
 
-                _logger.Information($"Deleting asset {asset}", new {asset.Name, asset.Id});
+                _logger.Information("Deleting asset {asset}", asset.ToLog());
                 asset.Delete();
             }
         }
@@ -215,13 +300,14 @@ namespace MediaSvcsIndexer2Demo
             Console.WriteLine("Job state changed event:");
             Console.WriteLine("  Previous state: " + e.PreviousState);
             Console.WriteLine("  Current state: " + e.CurrentState);
+            IJob job = (IJob)sender;
 
             switch (e.CurrentState)
             {
                 case JobState.Finished:
                     Console.WriteLine();
                     Console.WriteLine("Job is finished.");
-                    _logger.Debug("Job is finished");
+                    _logger.Debug("Job {job} is finished", job.ToLog());
                     Console.WriteLine();
                     break;
                 case JobState.Canceling:
@@ -233,15 +319,16 @@ namespace MediaSvcsIndexer2Demo
                 case JobState.Canceled:
                 case JobState.Error:
                     // Cast sender as a job.
-                    IJob job = (IJob)sender;
+                    //IJob job = (IJob)sender;
                     // Display or log error details as needed.
                     // LogJobStop(job.Id);
-                    Console.WriteLine($"{job.Name} job {e.CurrentState}");
+                    _logger.Error("{job} job {CurrentState}", job.ToLog(), e.CurrentState);
                     break;
                 default:
                     break;
             }
         }
     }
+
 
 }
